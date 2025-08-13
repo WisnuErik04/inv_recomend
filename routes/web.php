b988dc040b36012e5e6e8593047ba318824ebcd2
Erik Wisnu <erikwnugroho@gmail.com>
2025-08-13 19:42:07 +0700

    'edit'

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-<<<<<<< HEAD
-    return redirect('http://inv_recomend.test:8080/admin');
-=======
-    // return redirect('https://einv.my.id/admin');
->>>>>>> dde67a46819194ad6654a65e4b8d60cf9b35f7ab
+    return redirect('https://einv.my.id/admin');
 });
